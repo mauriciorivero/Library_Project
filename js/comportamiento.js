a9ad044cc5203acdaee18e9ad0ef3e3310830ecd
Mauricio Rivero <rivero.mauricio@gmail.com>
2025-11-13 18:59:52 -0500

    added the model and controller of the js

--- js/comportamiento.js
+++ js/comportamiento.js
@@ -1,1 +1,13 @@
-console.log("Los saluda el "+nombre+" con "+edad+" anos de edad");
+const Libro = require('./model/Libro');
+
+document.getElementById('botonGuardarLibro').onclick = function(){
+    const titulo = document.getElementById('titulo').value;
+    const isbn  = document.getElementById('isbn').value;
+    const autor = document.getElementById('autor').value;
+    const ano = document.getElementById('year').value;
+    const numeroPaginas = document.getElementById('numeroPaginas').value;
+    const esDisponible = document.getElementById('disponible').value;
+    
+    const libro = new Libro(titulo, isbn, autor, ano, numeroPaginas, esDisponible);
+    alert(libro.titulo+" "+libro.autor);
+}
--- model/controlBiblioteca.js
+++ model/controlBiblioteca.js
@@ -1,4 +1,6 @@
-const Libro = require('./Libro');
+/*const Libro = require('./Libro');
+
+
 
 const libro1 = new Libro('The lord of the rings','4442-652453-825525','J.R.R. Tolkien',1964,567,true);
 const libro2 = new Libro('The martian','5454576567','Andy Weir',2008,456,true);
@@ -8,3 +10,5 @@ console.log('El titulo es: '+libro2.title+' El isbn es: '+libro2.isbn+' Este lib
 
 libro1.returnBook();
 console.log('El titulo es: '+libro1.title+' El isbn es: '+libro1.isbn+' Este libro esta disponible? '+libro1.isAvailable);
+
+*/
